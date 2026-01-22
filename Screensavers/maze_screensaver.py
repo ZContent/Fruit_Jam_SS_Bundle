685cf270edf6783eca00a98c36c6bc8b57bfb247
Dan Cogliano <dan@zcontent.net>
2026-01-22 00:04:12 -0500

    Work in progress

--- Screensavers/maze_screensaver.py
+++ Screensavers/maze_screensaver.py
@@ -34,15 +34,14 @@ class maze:
     maze = []
     mazepath = []
 
-    def __init__(self, bmp):
-        self.bmp = bmp
-        self.reset()
+    def __init__(self):
+        self.reset(10,3)
 
     def reset(self,cs,lw):
         self.cellsize = cs
         self.lwidth = lw
         self.sizex = self.width // self.cellsize
-        self.sizey = self.height // cellsize
+        self.sizey = self.height // self.cellsize
         for i in range(self.sizex * self.sizey):
             if i == 0:
                 item = 0
@@ -52,13 +51,13 @@ class maze:
             	item = RIGHT
             else:
             	item = BOTTOM | RIGHT
-            maze.append(item)
-            mazepath.append(i)
+            self.maze.append(item)
+            self.mazepath.append(i)
 
-    def getX(item,width):
+    def getX(self,item,width):
         return item % width
 
-    def getY(item,width):
+    def getY(self,item,width):
         return item // width
 
     """
@@ -66,31 +65,33 @@ class maze:
     the maze.
     """
     def cell_join(self, cell1, cell2):
-
-        val = mazepath[cell2];
-        # set mazepath value
-        for incr in range(self.sizex*self.sizey - 1, -1, -1):
-            if self.mazepath[incr] == val:
-                self.mazepath[incr] = self.mazepath[cell1]
-        # set graphics
-        if cell1 + 1 == cell2: # open right wall
-            self.maze[cell1] = self.maze[cell1]&~RIGHT
-        else: # open bottom wall
-            self.maze[cell1] = self.maze[cell1]&~BOTTOM
+        print(f"cell_join {cell1},{cell2} of {len(self.mazepath)}")
+        if cell2 < self.sizex*self.sizey:
+            val = self.mazepath[cell2]
+            # set mazepath value
+            for incr in range(self.sizex*self.sizey - 1, -1, -1):
+                if self.mazepath[incr] == val:
+                    self.mazepath[incr] = self.mazepath[cell1]
+            # set graphics
+            if cell1 + 1 == cell2: # open right wall
+                self.maze[cell1] = self.maze[cell1]&~RIGHT
+            else: # open bottom wall
+                self.maze[cell1] = self.maze[cell1]&~BOTTOM
 
     """
     connect() attempts to connect two squares together, returning
     FALSE if the attempt failed
     """
     def connect(self, cell):
-        cellcheck = [2]
+        #print(f"connect({cell})")
+        cellcheck = [0,0]
 
         # check if cell is a border, if so, return false
         if (cell < self.sizex) or (cell%self.sizex == 0): # top or left line
             return False
         # determine order of cell attempts
-        cellcheck[0]=random.randomint(2);
-        cellcheck[1]=(cellcheck[0]+1)%2;
+        cellcheck[0]=random.randint(0,2)
+        cellcheck[1]=(cellcheck[0]+1)%2
         # check cells to see if can be connected
         for incr in range(2):
             if (self.getX(cell,self.sizex) == (self.sizex-1)) and (cellcheck[incr]==0):
@@ -108,16 +109,16 @@ class maze:
        (which, in turn, calls cell_join()) to generate the maze.
     """
     def generate(self):
-
+        print("generate()")
         while True:
             complete=True
             # pick a random cell
-            cell = self.sizex + random.randint(self.sizex*(self.sizey-1))
+            cell = self.sizex + random.randint(0,self.sizex*(self.sizey-1))
             # find the next cell that can be connected
             for incr in range(self.sizex * self.sizey):
 
-                checkcell=(incr+cell)%(self.sizex*self.sizey)
-                if (checkcell < sizex) or ((checkcell%sizex)==0):
+                checkcell=(incr+cell)%(self.sizex*(self.sizey-1))
+                if (checkcell < self.sizex) or ((checkcell%self.sizex)==0):
                     continue
                 if self.connect(checkcell):
                     complete = False
@@ -125,9 +126,9 @@ class maze:
             if complete == True:
                 break
         # break walls for start and end of maze, near center
-        cell = self.sizex/4 + random.randint(self.sizex/2)
+        cell = self.sizex/4 + random.randint(0,self.sizex//2)
         self.maze[cell] = self.maze[cell]&~BOTTOM
-        cell = self.sizex/4 + random.randint(self.sizex/2) + self.sizex*(self.sizey-1)
+        cell = self.sizex/4 + random.randint(0,self.sizex//2) + self.sizex*(self.sizey-1)
         self.maze[cell] = self.maze[cell]&~BOTTOM
 
 class MazeScreenSaver(Group):
@@ -141,8 +142,8 @@ class MazeScreenSaver(Group):
     def __init__(self):
         print("__init__")
         super().__init__()
-        self.init_graphics()
         self.maze = maze()
+        self.init_graphics()
 
     def init_graphics(self):
         print("init_graphics()")
@@ -154,10 +155,12 @@ class MazeScreenSaver(Group):
         bg_palette[3] = 0xff0000
 
         bg_tg = TileGrid(bitmap=bg_bmp, pixel_shader=bg_palette)
-        bg_bmp.fill(0)
+        bg_bmp.fill(WHITE)
         bg_group = Group(scale=1)
         bg_group.append(bg_tg)
         self.append(bg_group)
+        self.maze.generate()
+        self.display_maze(self.maze)
 
     def reset():
         print("reset()")
@@ -182,44 +185,58 @@ class MazeScreenSaver(Group):
         xcenter = 0 - maze.cellsize//2
         ycenter = 0 - maze.cellsize//2
         print(f"maze centering adjustment: {xcenter}, {ycenter}")
-        self.bg_bmp.fill(WHITE)
-        
-        // draw horizontal lines
+        self.bmp.fill(WHITE)
+
+        # draw horizontal lines
         for incry in range(maze.sizey):
             xstart = -1
             xend = -1
             for incrx in range(maze.sizex):
-                if maze[incry*sizex+incrx]&BOTTOM != 0 and xstart == -1:
+                if maze.maze[incry*maze.sizex+incrx]&BOTTOM != 0 and xstart == -1:
                     xstart = incrx
-                elif maze[incry*sizex+incrx]&BOTTOM == 0 and xtart != -1:
+                elif maze.maze[incry*maze.sizex+incrx]&BOTTOM == 0 and xstart != -1:
                     xend = incrx
-                    bitmaptools.fill_region(self.bg_bmp,xcenter + xstart*self.cellsize,ycenter + (incry+1)*self.cellsize,(xend - xstart)*self.cellsize+self.lwidth,self.lwidth,BLACK)
+                    x1 = xcenter + xstart*self.cellsize
+                    y1 = ycenter + (incry+1)*self.cellsize
+                    x2 = x1 + (xend - xstart)*self.cellsize+self.lwidth
+                    y2 = y1 + self.lwidth
+                    bitmaptools.fill_region(self.bmp,x1,y1,x2,y2,BLACK)
                     xstart = -1
                     xend = -1
             if xstart != -1:
               # finish line
-              xend = sizex
-              bitmaptools.fill_region(self.bg_bmp,xcenter + xstart*self.cellsize,ycenter + (incry+1)*self.cellsize,(xend - xstart)*self.cellsize+self.lwidth,self.lwidth,BLACK)
+              xend = maze.sizex
+              x1 = xcenter + xstart*self.cellsize
+              y1 = ycenter + (incry+1)*self.cellsize
+              x2 = x1 + (xend - xstart)*self.cellsize+self.lwidth
+              y2 = y1 + self.lwidth
+              bitmaptools.fill_region(self.bmp,x1,y1,x2,y2,BLACK)
               xstart = -1
               xend = -1
-        
+
         # draw vertical lines
 
         for incrx in range(maze.sizex):
             ystart = -1
             yend = -1
-            for(int incry = 0; incry < sizey; incry++)
             for incry in range(maze.sizey):
-                if maze[incry*sizex+incrx]&RIGHT !- 0 and incry > 0 and ystart == -1:
+                if maze.maze[incry*maze.sizex+incrx]&RIGHT != 0 and incry > 0 and ystart == -1:
                     ystart = incry
-                elif maze[incry*sizex+incrx]&RIGHT == 0 and incry > 0 and ystart != -1:
+                elif maze.maze[incry*maze.sizex+incrx]&RIGHT == 0 and incry > 0 and ystart != -1:
                     yend = incry
-                    bitmaptools.fill_region(self.bg_bmp, xcenter + (incrx+1)*self.cellsize,ycenter + ystart*self.cellsize,self.lwidth,(yend - ystart)*self.cellsize+self.lwidth,BLACK);
+                    x1 = xcenter + (incrx+1)*self.cellsize
+                    y1 = ycenter + ystart*self.cellsize
+                    x2 = x1 + self.lwidth
+                    y2 = y1 + (yend - ystart)*self.cellsize+self.lwidth
+                    bitmaptools.fill_region(self.bmp,x1,y1,x2,y2,BLACK)
                     ystart = -1
                     yend = -1
-            
+
             if ystart != -1:
                 # finish line
-                yend = sizey
-                bitmaptools.fill_region(self.bg_bmp, xcenter + (incrx+1)*self.cellsize,ycenter + ystart*self.cellsize,lwidth,(yend - ystart)*self.cellsize+self.lwidth,BLACK);
-
+                yend = maze.sizey
+                x1 = xcenter + (incrx+1)*self.cellsize
+                y1 = ycenter + ystart*self.cellsize
+                x2 = x1 + self.lwidth
+                y2 = y1 + (yend - ystart)*self.cellsize+self.lwidth
+                bitmaptools.fill_region(self.bmp,x1,y1,x2,y2,BLACK)
